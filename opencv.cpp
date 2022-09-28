2d454d2ca1b1207ed70e30fe6b91f5e2694ad185
LXLajdddjsqzhdxz <114549419+LXLajdddjsqzhdxz@users.noreply.github.com>
2022-09-28 11:13:00 +0800

    Add files via upload

---
+++ opencv.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>  
+#include <opencv2/core/core.hpp>  
+#include <opencv2/highgui/highgui.hpp>  
+using namespace cv;
+
+int main()
+{
+	// 读入一张图片（绝对路径，选择自己的图片路径）
+	Mat img = imread("F:\\test\\test1.jpg");
+	// 创建一个名为 "test"窗口    
+	namedWindow("test");
+	imshow("test", img);
+	waitKey(0);
+	return 0;
+}
